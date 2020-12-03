b3e76c1bc11fb8c13852b36b113ed1d1ef0d530d
notty-geek <sahilgirdhar410@gmail.com>
2020-12-03 23:02:23 +0530

    Added basic Java doc texts

--- src/GuessNumber.java
+++ src/GuessNumber.java
@@ -1,3 +1,21 @@
+/**
+* <h1>Guess Number Java Program</h1>
+* The Guess Number Java program implements an application that
+* requires an Guessing Input from user and match the random Number " .
+* <p>
+*
+* @author  Sahil, Kartik,Krinesh,Gurvinder
+* @version 1.0
+* @since   2020-03-10
+*/
+
+
+
+
+
+
+
+
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
@@ -6,7 +24,7 @@ public class GuessNumber {
     public static void main(String[] args) throws InterruptedException {
         Scanner s = new Scanner(System.in);
         Random r = new Random();
-
+         // Default Number Set is 5.
         int turns  = 5;
         final int NUMBER = r.nextInt(100);
 

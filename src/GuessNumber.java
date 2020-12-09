640fa08f8de9b2116d5a59231b994343ef355316
notty-geek <sahilgirdhar410@gmail.com>
2020-12-09 22:30:21 +0530

    Added Roounds Logic and Display Round Score

--- src/GuessNumber.java
+++ src/GuessNumber.java
@@ -11,31 +11,40 @@
 
 
 
-
-
-
-
-
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 public class GuessNumber {
 
     /**
-   * This is the main method which macthes the Guess Number entered by user.
+   * This is the main method which matches the Guess Number entered by user.
    * @param args Number.
-   * @return Print Statement Wheather the Number Guesses is true or False.
+   * @return Print Statement Whether the Number Guesses is true or False.
    * @exception IOException On input error.
    * @see IOException
    */
     
     public static void main(String[] args) throws InterruptedException {
         Scanner s = new Scanner(System.in);
-        Random r = new Random();
-         // Default Number Set is 5.
+       // Total Rounds In Game
+       int ROUNDS = 3;
+       int RoundWin = 0;
+       int count = 1;
+
+        System.out.println(
+            "A number is chosen"
+            + " between 1 to 100."
+            + "Guess the number"
+            + " within 5 trials and three rounds");
+        while (count <= ROUNDS) {
+         // // Given turn trials
         int turns  = 5;
-        final int NUMBER = r.nextInt(100);
+        int NUMBER = 1 + (int)(100
+                               * Math.random());
+        System.out.println("ROUND "+count);
+        //final int NUMBER = r.nextInt(100);
         while(turns != -1) {
+
 
 
 
@@ -46,23 +55,26 @@ public class GuessNumber {
             TimeUnit.SECONDS.sleep(1);
             System.out.println("Enter the number between 1 and 100. You have " + turns + " turns left");
             int gNumber = s.nextInt();
-            System.out.println("---------------------------------------------------------------------");
+           
 
 
             if(gNumber == NUMBER){
+       
+            	
                 System.out.println("-------------------------------------------------------------------");
                 System.out.println("Congratulations! Correct Answer");
+                ++RoundWin;
                 break;
             }
             else if(gNumber > NUMBER){
-                System.out.println("-------------------------------------------------------------------");
+
                 System.out.println("Wrong Answer! Try going lower");
                 turns -= 1;
                 TimeUnit.SECONDS.sleep(1);
                 
             }
             else if(gNumber < NUMBER){
-                System.out.println("-------------------------------------------------------------------");
+
                 System.out.println("Wrong Answer! Try going higher");
                 turns -= 1;
                 TimeUnit.SECONDS.sleep(1);
@@ -70,6 +82,10 @@ public class GuessNumber {
             }
             
         }
+
+        ++count;
+        }
+       System.out.println("Game Over. Rounds Score "+RoundWin+" / "+ROUNDS);
         
 
     }
